8debb9418b059512b4454885004c03796fb0f828
Laura Cabayol <lauracabayol@gmail.com>
2024-01-26 00:20:29 +0100

    quick explanation about the changes in the predicting functions

--- notebooks/Tutorial_emulator.py
+++ notebooks/Tutorial_emulator.py
@@ -6,11 +6,11 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.15.2
+#       jupytext_version: 1.14.5
 #   kernelspec:
-#     display_name: ForestFlow
+#     display_name: emulators
 #     language: python
-#     name: forestflow
+#     name: emulators
 # ---
 
 # %% [markdown]
@@ -71,10 +71,7 @@ p3d_emu = P3DEmulator(
     adamw=True,
     nLayers_inn=12,  # 15
     Archive=Archive3D,
-    use_chains=False,
-    chain_samp=100_000,
-    folder_chains="/data/desi/scratch/jchavesm/p3d_fits_new/",
-)
+    )
 
 # %% [markdown]
 # ## TEST EMULATOR
@@ -127,18 +124,43 @@ z_test = 3
 
 # %%
 test_sim = central = Archive3D.get_testing_data(
-    "mpg_central", force_recompute_plin=True
+    "mpg_central", force_recompute_plin=False
 )
 dict_sim = [d for d in test_sim if d["z"] == z_test and d["val_scaling"] == 1]
 
+# %% [markdown]
+# If we do not specify the k numbers and mu modes where the P3D is computed, it will use ones that are in the training data.
+# You can find them in p3d_emu.k_Mpc_masked and p3d_emu.mu_masked.
+#
+#
+
 # %%
 p3d_pred, p3d_cov = p3d_emu.predict_P3D_Mpc(
-    sim_label="mpg_central", z=z_test, test_sim=dict_sim, return_cov=True
+    sim_label="mpg_central",
+    z=z_test, 
+    test_sim=dict_sim[0], 
+    return_cov=True
 )
+
+# %% [markdown]
+# You can also specify concrete values for the k number where the P3D is computed.
+
+# %%
+p3d_pred, p3d_cov = p3d_emu.predict_P3D_Mpc(
+    sim_label="mpg_central", 
+    z=z_test, 
+    test_sim=dict_sim[0], 
+    k_Mpc=p3d_emu.k_Mpc_masked,
+    mu = p3d_emu.mu_masked,
+    return_cov=True
+)
+
+# %% [markdown]
+# So far, this is not implemented for the predict_P1D_Mpc, which always computes the p1d default values in the simulations. This is currenly under internal discussion. You can access the kpar values of the emulated p1d as: p1d_k = dict_sim[0]['k_Mpc'][p3d_emu.k1d_mask] 
 
 # %%
 p1d_pred, p1d_cov = p3d_emu.predict_P1D_Mpc(
-    sim_label="mpg_central", z=z_test, test_sim=dict_sim, return_cov=True
+    sim_label="mpg_central", z=z_test, test_sim=dict_sim[0], return_cov=True
 )
 
 # %%
