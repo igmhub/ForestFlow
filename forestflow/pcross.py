9ffc446a3876bce333c1d4b979f24c90f305174d
Martine <mlokken394@gmail.com>
2024-02-28 17:56:06 +0100

    added two separate functions, added tutorial examples and tests

--- forestflow/pcross.py
+++ forestflow/pcross.py
@@ -1,32 +1,64 @@
 import numpy as np
+from scipy.integrate import simpson
 
+def P1D_Mpc(P3D_Mpc, z, ln_k_perp, kpars, P3D_mode='pol',  **P3D_kwargs):
+    """
+    Compute P1D by integrating P3D in terms of ln(k_perp) using a fast method.
 
-def get_P1D()... # add this so that it can be called below.
+    Parameters:
+        z (float): Redshift.
+        ln_k_perp (array-like): Array of natural logarithms of the perpendicular wavenumber.
+        kpars (array-like): Array of parallel wavenumbers.
+        parameters (dict, optional): Additional parameters for the model. Defaults to {}.
 
+    Returns:
+        array-like: Computed values of P1D.
+    """
+
+    # get interval for integration
+    dlnk = ln_k_perp[1] - ln_k_perp[0]
+
+    # get function to be integrated
+    # it is equivalent of the inner loop of _P1D_lnkperp
+    k_perp = np.exp(ln_k_perp)
+    k = np.sqrt(kpars[np.newaxis, :] ** 2 + k_perp[:, np.newaxis] ** 2)
+    mu = kpars[np.newaxis, :] / k
+    k = k.swapaxes(0, 1)
+    mu = mu.swapaxes(0, 1)
+    
+    
+    fact = (1 / (2 * np.pi)) * k_perp[:, np.newaxis] ** 2
+    fact = fact.swapaxes(0, 1)
+    if P3D_mode == 'pol':
+        p3d_fix_k_par = P3D_Mpc(z, k, mu,  **P3D_kwargs) * fact
+    elif P3D_mode == 'cart':
+        # tile
+        kperp2d  = np.tile(kperps[:, np.newaxis], len(kpars)) # mu grid for P3D
+        kpar2d   = np.tile(kpars[:, np.newaxis], len(k_perp)).T
+        p3d_fix_k_par = P3D_Mpc(z, kpar2d, kperp2d, **P3D_kwargs) * fact
+    # perform numerical integration
+    p1d = simpson(p3d_fix_k_par, ln_k_perp, dx=dlnk, axis=1)
+
+    return p1d
 
 def get_Px(
     kpars,
     P3D,
     z,
-    fast=False,
-    **P3D_kwargs,
-    P1D=None
+    P3D_mode='pol',
+    **P3D_kwargs
 ):
     """ Calculates P_cross, the power for a given k_parallel mode from pairs of lines-of-sight separated by perpendicular distance rperp, given a 3D power spectrum
     Calculation is done with the hankl transform.
     This code is a simpler version with fewer changeable parameters. See get_Px_detailed for a version with all possible tweaks.
     Required Parameters:
         kpars (array): array of k parallel (usually log-spaced)    
-        P3D (function): Function that takes arguments 
-        params (dictionary): parameters for the Arinyo parameters. If not given, they will be set to default values
+        P3D (function): Function that takes arguments
         z (float): single redshift to evaluate
     Optional Parameters:
-        min_rperp, max_rperp (float): desired range of rperp values to return
-        min_kperp, max_kperp (float): range of kperp values to use in the calculation. Decreasing this range can cause unwanted artifacts
-        Nsteps_kperp (int): number of kperps for the hankl transform (and number of output rperp). Decreasing this speeds up calculation but decreases accuracy
-        trans_to_p1d (bool): determines whether to transition to the P1D result at low rperp
-        fast_transition (bool): if true, the transition to P1D is done faster, without interpolation, but there will be a discontinuity
-        P1D: precomputed P1D evaluated at kpars
+        P3D_mode: 'pol' or 'cart' for polar or cartesian. 'pol' assumes that the function takes parameters z and an array of k and mu. 'cart' assumes that the parameters are kpar and kperp, both arrays.
+        fast (bool): if true, the transition to P1D is done faster, without interpolation, but there will be a discontinuity
+        **P3D_kwargs: optional named arguments to be passed to the P3D function.
     Returns:
         rperp, Px_per_rperp, Px_per_kpar
         rperp: array of log-space r-perpendicular (separation in Mpc)
@@ -34,49 +66,52 @@ def get_Px(
     """
     import hankl
     from scipy.interpolate import CubicSpline
-
+    nkperp = 2**16
+    nkpar  = len(kpars)
+    kperps = np.logspace(
+        np.log10(10.0**-20), np.log10(10.0**3), nkperp
+    )  # set up an array of kperp
+    # tile
+    kperp2d  = np.tile(kperps[:, np.newaxis], nkpar) # mu grid for P3D
+    kpar2d   = np.tile(kpars[:, np.newaxis], nkperp).T
+    k2d = np.sqrt(kperp2d**2 + kpar2d**2)
+    mu2d = kpar2d / k2d
+    if P3D_mode == 'cart':
+        # assume P3D is a function of (z, kpar, kperp)
+        P3D_eval = P3D(z=z, kpar=kpar2d, kperp=kperp2d, **P3D_kwargs)
+    elif P3D_mode == 'pol':
+        # assume P3D is a function of (z, k, mu)
+        P3D_eval = P3D(z=z, k=k2d, mu=mu2d, **P3D_kwargs)
+    P1D = P1D_Mpc(P3D, z, np.linspace(np.log(0.001), np.log(100), 99), kpars, P3D_mode = P3D_mode, **P3D_kwargs) # get P1D
     Px_per_kpar = []
-    for kpar in kpars:  # for each value of k parallel to evaluate Px at
-        kperps = np.logspace(
-            np.log10(10.0**-20), np.log10(10.0**3), 2**16
-        )  # set up an array of kperp
-        kpars_prime = np.full(
-            len(kperps), kpar
-        )  # each kperp gets the same kpar for this iteration -- make a full array of the kpar value
-        k = np.sqrt(kpars_prime**2 + kperps**2)  # get the corresponding k array
-        mu = kpars_prime / k  # array of corresponding mu
-        # P3D = arinyo.P3D_Mpc(z, k, mu, params)  
-        P3D_eval = P3D(z=z, k=k, mu=mu, **P3D_kwargs) * kperps # get the P3D
-        func     = P3D_eval * kperps
+    for ik, kpar in enumerate(kpars):  # for each value of k parallel to evaluate Px at
+        k = k2d[:,ik]
+        mu = mu2d[:,ik]
+        P3D_kpar = P3D_eval[:,ik] # get the P3D
+        func     = P3D_kpar * kperps
         rperp, LHS = hankl.FFTLog(
             kperps, func, q=0, mu=0
         )  # returns an array of log-spaced rperps, and the Hankel Transform
         Px = LHS / rperp / (2*np.pi)  # Divide out by remaining factor to get Px
-        # transition
-        if precomputed_P1D is None:
-            P1D = # insert a function to get P1D
-        if fast:
-            replace = rperp < 0.04
-            Px[replace] = p1d
-        else:
-            # replace the values left of the minimum
-            replace = rperp < 0.02
-            # return the P1D result for that kpar
-            Px[replace] = p1d
-            # between rperp = 0.02 and 0.08, interpolate from P1D to Px values
-            idxmin = (np.abs(rperp - 0.02)).argmin()
-            idxmax = (np.abs(rperp - 0.08)).argmin()
-            rperps_interp = rperp[idxmin:idxmax]
-            Px_tointerp = np.delete(Px, np.arange(idxmin, idxmax))
-            rperp_tointerp = np.delete(rperp, np.arange(idxmin, idxmax))
-            interpmin = np.abs(rperp_tointerp - 0.005).argmin()
-            interpmax = np.abs(rperp_tointerp - 0.2).argmin()
-            cs = CubicSpline(
-                rperp_tointerp[interpmin:interpmax],
-                Px_tointerp[interpmin:interpmax],
-            )
-            Px_interpd = cs(rperps_interp)
-            Px = np.insert(Px_tointerp, idxmin, Px_interpd)
+        # transition    
+        # replace the values left of the minimum
+        replace = rperp < 0.02
+        # return the P1D result for that kpar
+        Px[replace] = P1D[ik]
+        # between rperp = 0.02 and 0.08, interpolate from P1D to Px values
+        idxmin = (np.abs(rperp - 0.02)).argmin()
+        idxmax = (np.abs(rperp - 0.08)).argmin()
+        rperps_interp = rperp[idxmin:idxmax]
+        Px_tointerp = np.delete(Px, np.arange(idxmin, idxmax))
+        rperp_tointerp = np.delete(rperp, np.arange(idxmin, idxmax))
+        interpmin = np.abs(rperp_tointerp - 0.005).argmin()
+        interpmax = np.abs(rperp_tointerp - 0.2).argmin()
+        cs = CubicSpline(
+            rperp_tointerp[interpmin:interpmax],
+            Px_tointerp[interpmin:interpmax],
+        )
+        Px_interpd = cs(rperps_interp)
+        Px = np.insert(Px_tointerp, idxmin, Px_interpd)
         min_rperp, max_rperp = 10**-2, 100
         if min_rperp > min(rperp):
             rperp_minidx = np.argmin(abs(rperp - min_rperp))
@@ -89,18 +124,22 @@ def get_Px(
     Px_per_kpar = np.asarray(Px_per_kpar)
     return rperp, Px_per_kpar
 
+
 def get_Px_detailed(
     kpars,
-    arinyo,
+    P3D,
     z,
-    min_rperp=0.01,
-    max_rperp=30,
+    P3D_mode='pol',
+    min_rperp=10**-2,
+    max_rperp=100,
     min_kperp=10.0**-20,
     max_kperp=10.0**3,
-    Nsteps_kperp=2**16,
+    nkperp=2**16,
     trans_to_p1d=True,
+    interpmin = 0.005,
+    interpmax = 0.2,
     fast_transition=False,
-    params=None,
+    **P3D_kwargs
 ):
     """ Calculates P_cross, the power for a given k_parallel mode from pairs of lines-of-sight separated by perpendicular distance rperp, given a 3D power spectrum
     Calculation is done with the hankl transform.
@@ -108,7 +147,6 @@ def get_Px_detailed(
     Required Parameters:
         kpars (array): array of k parallel (usually log-spaced)    
         arinyo (ArinyoModel instance): Arinyo model with a pre-chosen cosmology
-        params (dictionary): parameters for the Arinyo parameters. If not given, they will be set to default values
         z (float): single redshift to evaluate
     Optional Parameters:
         min_rperp, max_rperp (float): desired range of rperp values to return
@@ -125,51 +163,57 @@ def get_Px_detailed(
     import hankl
     from scipy.interpolate import CubicSpline
 
-    if params is None:
-        params = arinyo.default_params
-    if min_rperp > 0.08:
-        trans_to_p1d = False  # not necessary to transition to the P1D result if minimum requested rperp is larger than 0.08
+
+    nkpar  = len(kpars)
+    kperps = np.logspace(
+        np.log10(min_kperp), np.log10(max_kperp), nkperp
+    )  # set up an array of kperp
+    # tile
+    kperp2d  = np.tile(kperps[:, np.newaxis], nkpar) # mu grid for P3D
+    kpar2d   = np.tile(kpars[:, np.newaxis], nkperp).T
+    k2d = np.sqrt(kperp2d**2 + kpar2d**2)
+    mu2d = kpar2d / k2d
+    if P3D_mode == 'cart':
+        # assume P3D is a function of (z, kpar, kperp)
+        P3D_eval = P3D(z=z, kpar=kpar2d, kperp=kperp2d, **P3D_kwargs)
+    elif P3D_mode == 'pol':
+        # assume P3D is a function of (z, k, mu)
+        P3D_eval = P3D(z=z, k=k2d, mu=mu2d, **P3D_kwargs)
+    P1D = P1D_Mpc(P3D, z, np.linspace(np.log(0.001), np.log(100), 99), kpars, P3D_mode = P3D_mode, **P3D_kwargs) # get P1D
     Px_per_kpar = []
-    for kpar in kpars:  # for each value of k parallel to evaluate Px at
-        kperps = np.logspace(
-            np.log10(min_kperp), np.log10(max_kperp), Nsteps_kperp
-        )  # set up an array of kperp
-        kpars_prime = np.full(
-            len(kperps), kpar
-        )  # each kperp gets the same kpar for this iteration -- make a full array of the kpar value
-        k = np.sqrt(kpars_prime**2 + kperps**2)  # get the corresponding k array
-        mu = kpars_prime / k  # array of corresponding mu
-        P3D = arinyo.P3D_Mpc(z, k, mu, params)  # get the P3D
-        func = P3D * kperps
+
+    for ik, kpar in enumerate(kpars):  # for each value of k parallel to evaluate Px at
+        k = k2d[:,ik]
+        mu = mu2d[:,ik]
+        P3D_kpar = P3D_eval[:,ik] # get the P3D
+        func     = P3D_kpar * kperps
         rperp, LHS = hankl.FFTLog(
             kperps, func, q=0, mu=0
         )  # returns an array of log-spaced rperps, and the Hankel Transform
         Px = LHS / rperp / (2*np.pi)  # Divide out by remaining factor to get Px
-        # transition
-        if trans_to_p1d:
-            p1d = arinyo.P1D_Mpc(z, np.array([kpar]), parameters=params)
-            if fast_transition:
-                replace = rperp < 0.04
-                Px[replace] = p1d
-            else:
-                # replace the values left of the minimum
-                replace = rperp < 0.02
-                # return the P1D result for that kpar
-                Px[replace] = p1d
-                # between rperp = 0.02 and 0.08, interpolate from P1D to Px values
-                idxmin = (np.abs(rperp - 0.02)).argmin()
-                idxmax = (np.abs(rperp - 0.08)).argmin()
-                rperps_interp = rperp[idxmin:idxmax]
-                Px_tointerp = np.delete(Px, np.arange(idxmin, idxmax))
-                rperp_tointerp = np.delete(rperp, np.arange(idxmin, idxmax))
-                interpmin = np.abs(rperp_tointerp - 0.005).argmin()
-                interpmax = np.abs(rperp_tointerp - 0.2).argmin()
-                cs = CubicSpline(
-                    rperp_tointerp[interpmin:interpmax],
-                    Px_tointerp[interpmin:interpmax],
-                )
-                Px_interpd = cs(rperps_interp)
-                Px = np.insert(Px_tointerp, idxmin, Px_interpd)
+        # transition    
+        if fast_transition:
+            replace = rperp < 0.04
+            Px[replace] = P1D[ik]
+        else:
+            # replace the values left of the minimum
+            replace = rperp < (0.02)
+            # return the P1D result for that kpar
+            Px[replace] = P1D[ik]
+            # between rperp = 0.02 and 0.08, interpolate from P1D to Px values
+            idxmin = (np.abs(rperp - 0.02)).argmin()
+            idxmax = (np.abs(rperp - 0.08)).argmin()
+            rperps_interp = rperp[idxmin:idxmax]
+            Px_tointerp = np.delete(Px, np.arange(idxmin, idxmax))
+            rperp_tointerp = np.delete(rperp, np.arange(idxmin, idxmax))
+            interpmin_id = np.abs(rperp_tointerp - interpmin).argmin()
+            interpmax_id = np.abs(rperp_tointerp - interpmax).argmin()
+            cs = CubicSpline(
+                rperp_tointerp[interpmin_id:interpmax_id],
+                Px_tointerp[interpmin_id:interpmax_id],
+            )
+            Px_interpd = cs(rperps_interp)
+            Px = np.insert(Px_tointerp, idxmin, Px_interpd)
         if min_rperp > min(rperp):
             rperp_minidx = np.argmin(abs(rperp - min_rperp))
         if max_rperp < max(rperp):
@@ -179,5 +223,4 @@ def get_Px_detailed(
         Px_per_kpar.append(Px[rperp_minidx:rperp_maxidx])
         rperp = rperp[rperp_minidx:rperp_maxidx]
     Px_per_kpar = np.asarray(Px_per_kpar)
-
     return rperp, Px_per_kpar
--- notebooks/Tutorial_Pcross.py
+++ notebooks/Tutorial_Pcross.py
@@ -7,9 +7,9 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.14.5
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: lace
 #     language: python
-#     name: python3
+#     name: lace
 # ---
 
 # # Tutorial for how to calculate $P_\times$
@@ -29,38 +29,37 @@ rcParams["mathtext.fontset"] = "stix"
 rcParams["font.family"] = "STIXGeneral"
 # import P3D theory
 from lace.cosmo import camb_cosmo
-from ForestFlow.model_p3d_arinyo import get_linP_interp
-from ForestFlow.model_p3d_arinyo import ArinyoModel
-import timeit
-sys.path.append("../scripts")
-sys.path.append("../forestflow")
-from pcross import get_Px
+from forestflow.model_p3d_arinyo import get_linP_interp
+from forestflow.model_p3d_arinyo import ArinyoModel
+import time
+# %load_ext autoreload
+# %autoreload 2
+from forestflow.pcross import get_Px
 import hankl
 
 # First, choose a redshift and $k$ range. Initialize an instance of the Arinyo class for this redshift given cosmology calculations from Camb.
 
-from lya_pk.model_p3d_arinyo import get_linP_interp
-from lya_pk.model_p3d_arinyo import ArinyoModel
-zs = np.array([2, 2.5])
-cosmo = camb_cosmo.get_cosmology()
-camb_results = camb_cosmo.get_camb_results(cosmo, zs=zs, camb_kmax_Mpc=100)
-arinyo = ArinyoModel(cosmo=cosmo, camb_results=camb_results, zs=zs, camb_kmax_Mpc=100)
-params = arinyo.default_params
+zs = np.array([2,2.5]) # set target redshift
+cosmo = camb_cosmo.get_cosmology() # set default cosmo
+camb_results = camb_cosmo.get_camb_results(cosmo, zs=zs, camb_kmax_Mpc=200) # set default cosmo
+arinyo = ArinyoModel(cosmo=cosmo, camb_results=camb_results, zs=zs, camb_kmax_Mpc=200) # set model
+arinyo.default_params
 
 # ## Plot the 3D power spectrum
 
 # +
-nn_k = 200
-nn_mu = 10
-k = np.logspace(-4, 8, nn_k)
-kpar = np.logspace(-4, 3, nn_k)
+nn_k = 200 # number of k bins
+nn_mu = 10 # number of mu bins
+k = np.logspace(-1.5, 2, nn_k)
 mu = np.linspace(0, 1, nn_mu)
-k2d = np.tile(k[:, np.newaxis], nn_mu)
-mu2d = np.tile(mu[:, np.newaxis], nn_k).T
+k2d = np.tile(k[:, np.newaxis], nn_mu) # k grid for P3D
+mu2d = np.tile(mu[:, np.newaxis], nn_k).T # mu grid for P3D
 
-plin = arinyo.linP_Mpc(zs[0], k)
-p3d = arinyo.P3D_Mpc(zs[0], k2d, mu2d, params)
-p1d = arinyo.P1D_Mpc(zs[0], kpar, parameters=params)
+kpar = np.logspace(-1, np.log10(5), nn_k) # kpar for P1D
+
+plin = arinyo.linP_Mpc(zs[0], k) # get linear power spectrum at target z
+p3d = arinyo.P3D_Mpc(zs[0], k2d, mu2d, arinyo.default_params) # get P3D at target z
+p1d = arinyo.P1D_Mpc(zs[0], kpar, parameters=arinyo.default_params) # get P1D at target z
 # -
 
 for ii in range(p3d.shape[1]):
@@ -70,6 +69,17 @@ plt.ylabel(r'$P/P_{\rm lin}$')
 plt.xlim([10**-1,10**8])
 plt.ylim([10**-10,10])
 plt.legend()
+
+from forestflow.pcross import get_Px
+
+
+rperp, Px_per_kpar = get_Px(
+    kpar,
+    arinyo.P3D_Mpc,
+    zs[0],
+    P3D_mode='pol',
+    fast=False,
+    **{'pp':arinyo.default_params})
 
 # # Calculate $P_\times$ for a series of $k_\parallel$.
 #
@@ -87,24 +97,29 @@ plt.legend()
 # This integral is adding up all of the different $k_{\perp}$ modes that enter into the real-space delta fluctuations at a given $\theta$ separation.
 #
 # To solve, it can be rearranged to become:
-# $$ P_{\times}(z,k_{\parallel}, r_\perp) = \frac{1}{2\pi} \int_{k_{\parallel}}^{\infty} k dk j_0 (k_\perp r_\perp) P_\mathrm{3D}(z, k, \mu)$$
+# $$ P_{\times}(z,k_{\parallel}, r_\perp) = \frac{1}{2\pi} \int_{k_{\parallel}}^{\infty} k dk J_0 (k_\perp r_\perp) P_\mathrm{3D}(z, k, \mu)$$
 #
 # The get_Px function performs the Hankel transform to integrate P3D.
 #
 # Going the other way around, if one wanted to compute $P_\mathrm{3D}$ from a $P_\times$ measurement, we would do
 # $$ P_\mathrm{3D}(z,k_{\perp}, k_{\parallel}) = 2\pi \int_{0}^{\infty} d\theta J_0 (k_\perp \theta) \theta P_\times (z, \theta, k_{\parallel})$$
 #
-# <!-- $$ P_{\times}(z,k_{\parallel}, \theta) = \frac{1}{2\pi} \int_{k_{\parallel}}^{\infty} k dk j_0 (k_\perp \theta) P(z, k, \mu)$$ -->
+# <!-- $$ P_{\times}(z,k_{\parallel}, \theta) = \frac{1}{2\pi} \int_{k_{\parallel}}^{\infty} k dk J_0 (k_\perp \theta) P(z, k, \mu)$$ -->
 #
 
+# choose some values of k parallel to compute
 kpars_Px  = np.logspace(-3, np.log10(20), 100)
-rperp_fid,Px_per_kpar_fid = get_Px(kpars_Px, arinyo, zs[1], min_kperp=10.**-20, max_kperp=10.**3, Nsteps_kperp=10000, trans_to_p1d=False)
+rperp,Px_per_kpar = get_Px(kpars_Px,
+    arinyo.P3D_Mpc,
+    zs[0],
+    P3D_mode='pol',
+    **{'pp':arinyo.default_params})
 
 
 # # Plot $P_\times$ as a function of $r_\perp$
 # At very low rperp, $P_\times$ should match with P1D. We can plot the P1D predictions for the same model for few values of $k_\parallel$, to test the integration.
 
-p1d_comparison = arinyo.P1D_Mpc(zs[1], kpars_Px, parameters=params)
+p1d_comparison = arinyo.P1D_Mpc(zs[0], kpars_Px, parameters=arinyo.default_params) # get the P1D comparison
 
 # +
 # first, check if the fiducial matches P1D at the low end
@@ -114,12 +129,12 @@ delta = 0
 
 kpars_to_plot  = np.arange(0,len(kpars_Px),10).astype(int)
 kpar_plot = kpars_Px[kpars_to_plot]
-for ik, Px in enumerate(Px_per_kpar_fid[kpars_to_plot]):
-    ax[0].plot(rperp_fid, Px, label=f'$k_\parallel$={round(kpar_plot[ik],3)}', c=cmap(ik/len(kpars_to_plot)))
-    ax[0].plot(rperp_fid,np.full(len(rperp_fid), p1d_comparison[kpars_to_plot][ik]), c=cmap(ik/len(kpars_to_plot)), linestyle='--')
+for ik, Px in enumerate(Px_per_kpar[kpars_to_plot]):
+    ax[0].plot(rperp, Px, label=f'$k_\parallel$={round(kpar_plot[ik],3)}', c=cmap(ik/len(kpars_to_plot)))
+    ax[0].plot(rperp,np.full(len(rperp), p1d_comparison[kpars_to_plot][ik]), c=cmap(ik/len(kpars_to_plot)), linestyle='--')
 
-    pctdiff = ((np.full(len(rperp_fid), p1d_comparison[kpars_to_plot][ik])-Px)/Px)*100
-    ax[1].plot(rperp_fid, pctdiff, c=cmap(ik/len(kpars_to_plot)))
+    pctdiff = ((np.full(len(rperp), p1d_comparison[kpars_to_plot][ik])-Px)/Px)*100
+    ax[1].plot(rperp, pctdiff, c=cmap(ik/len(kpars_to_plot)))
 
 ax[0].legend()
 ax[1].set_xlabel(r"$r_\perp$ [Mpc]")
@@ -127,11 +142,145 @@ ax[0].set_ylabel(r"$P_\times$ [Mpc]")
 ax[1].set_ylabel("% diff")
 plt.xscale("log")
 # plt.yscale("log")
-ax[0].set_xlim([.001,30])
-ax[0].set_ylim([0,0.6])
+ax[0].set_xlim([.01,1])
+ax[0].set_ylim([0,np.amax(Px_per_kpar)+.1])
 ax[1].set_ylim([-1,1])
-# plt.suptitle(r"$N_\mathrm{steps} = 5000, k_\perp^\mathrm{min}=10^{-20}, k_\perp^\mathrm{max}=10^3$")
+plt.suptitle(r"$P_\times$ vs P1D, default settings")
 
 # -
+
+# # Now let us look at $P_\times$ in a different way, as a function of $k_\parallel$ for different $r_\perp$ values
+
+Px_per_rperp = Px_per_kpar.T
+
+
+# series of rperp we're interested in
+rperps_toplot = np.array([0, 0.2,0.972,2.204,3.444,5.941])/cosmo.h # Example, from Abdul-Karim et al 2023
+print("Trying to plot r_perp =", rperps_toplot, "Mpc")
+rperps_choice = [(np.abs(rperp - rperp_plot)).argmin() for rperp_plot in rperps_toplot]
+print("Going to grab indices:", rperps_choice)
+print("Going to plot:", rperp[rperps_choice], "Mpc")
+
+# +
+# check that the first one has no fractional difference
+fig,ax = plt.subplots(nrows=2,ncols=1, figsize=[8,5], gridspec_kw={'height_ratios':[3,1]}, sharex=True)
+delta = 0
+ax[0].plot(kpars_Px, p1d_comparison, label='arinyo model 1D', color='k')
+ax[0].plot(kpars_Px, Px_per_rperp[rperps_choice][0], linestyle='dashed', color='yellow', label='first Px')
+
+
+pctdiff = (Px_per_rperp[rperps_choice][0]-p1d_comparison)/p1d_comparison*100
+ax[1].plot(kpars_Px, pctdiff)
+
+ax[0].legend()
+ax[1].set_xlim([0.1,20])
+ax[0].set_ylim([10**-7,1.1])
+ax[0].set_yscale("log")
+ax[0].set_xscale("log")
+ax[0].set_ylabel(r"$P_\times$ [Mpc/$h$]")
+ax[1].set_xlabel(r"$k_{\parallel}$ [$h$ Mpc$^{-1}$]")
+# they match perfectly--good
+
+# +
+# reproduce plot from Abdul-Karim et al 2023
+fig,ax = plt.subplots(1,1)
+colors=['blue','orange','green','red','yellow','purple']
+plt.title(r"$P_\times$ from FFTLog integration")
+rperps_choice_r = rperp[rperps_choice]
+print(rperps_choice_r)
+for r, Px in enumerate(Px_per_rperp[rperps_choice]):
+    plt.loglog(kpars_Px/cosmo.h, Px*cosmo.h, 'o', label=f"$r_{{\perp}}=${round(rperps_choice_r[r],3)*0.675} Mpc/h", ms=5, c=colors[r])
+
+ax.set_xlim([0.4,20])
+ax.set_ylim([10**-7,10**1])
+ax.loglog(kpars_Px/cosmo.h, p1d_comparison*cosmo.h, label='arinyo model 1D', color='0.8')
+ax.set_ylabel(r"$P_\times$ [Mpc/$h$]")
+ax.set_xlabel(r"$k_{\parallel}$ [$h$ Mpc$^{-1}$]")
+ax.set_yticks([10**-7,10**-5,10**-3,10**-1,10])
+ax.grid()
+plt.legend()
+
+# +
+# # here we can see whether the fast mode is any quicker. First, without it:
+# start = time.time()
+# rperp_fid,Px_per_kpar = get_Px(kpars_Px,
+#     arinyo.P3D_Mpc,
+#     zs[0],
+#     P3D_mode='pol',
+#     fast=False,
+#     **{'pp':arinyo.default_params})
+# end = time.time()
+# print(end-start)
+
+# # Next, with it:
+# start = time.time()
+# rperp_fid,Px_per_kpar = get_Px(kpars_Px,
+#     arinyo.P3D_Mpc,
+#     zs[0],
+#     P3D_mode='pol',
+#     fast=True,
+#     **{'pp':arinyo.default_params})
+# end = time.time()
+# print(end-start)
+# # does not make any difference, results were 26.034342288970947 vs 26.775516986846924 (actually slower)
+
+# -
+
+# # If you want to use the pcross function with several variations, we can use Px_detailed
+
+from forestflow.pcross import get_Px_detailed
+
+rperp_alt,Px_per_kpar_alt = get_Px_detailed(
+    kpars_Px,
+    arinyo.P3D_Mpc,
+    zs[0],
+    P3D_mode='pol',
+    min_rperp=10**-2,
+    max_rperp=100,
+    min_kperp=10.0**-20,
+    max_kperp=10.0**3,
+    nkperp=2**14,
+    trans_to_p1d=True,
+    interpmin = 0.005,
+    interpmax = 0.2,
+    fast_transition=False, 
+    **{'pp':arinyo.default_params}
+)
+
+# +
+# check accuracy with respect to fiducial
+
+same_rperp = [np.argmin(abs(rperp-rp)) for rp in rperp_alt]
+
+fig,ax = plt.subplots(nrows=2,ncols=1, figsize=[8,5], gridspec_kw={'height_ratios':[3,1]}, sharex=True)
+delta = 0
+
+kpars_to_plot  = np.arange(0,len(kpars_Px),10).astype(int)
+kpar_plot = kpars_Px[kpars_to_plot]
+for ik, Px in enumerate(Px_per_kpar[kpars_to_plot]):
+    ax[0].plot(rperp, Px, label=f'$k_\parallel$={round(kpar_plot[ik],3)}', c=cmap(ik/len(kpars_to_plot)))
+    ax[0].plot(rperp_alt, Px_per_kpar_alt[kpars_to_plot[ik]], c=cmap(ik/len(kpars_to_plot)), linestyle='dashed')
+    pctdiff = (Px_per_kpar_alt[kpars_to_plot[ik]]-Px[same_rperp])/Px[same_rperp]*100
+    ax[1].plot(rperp_alt, pctdiff, c=cmap(ik/len(kpars_to_plot)))
+    # add a tolerance
+    if len(rperp_alt[rperp_alt<80][pctdiff[rperp_alt<80]>0.1])>0:
+        print("minimum", np.amin(rperp_alt[pctdiff>.1]), "tolerance exceeded.")
+        print("maximum", np.amax(rperp_alt[rperp_alt<80][pctdiff[rperp_alt<80]>.1]), "tolerance exceeded.")
+
+ax[0].legend()
+ax[1].set_xlabel(r"$r_\perp$ [Mpc]")
+ax[0].set_ylabel(r"$P_\times$ [Mpc]")
+ax[1].set_ylabel("% diff")
+plt.xscale("log")
+# plt.yscale("log")
+ax[0].set_xlim([.01,30])
+ax[0].set_ylim([0,0.6])
+ax[1].set_ylim([-1,1])
+plt.suptitle(r"$N_\mathrm{steps} = 2^16, k_\perp^\mathrm{min}=10^{-20}, k_\perp^\mathrm{max}=10^3$")
+
+
+# -
+
+# ## We can see that by lowering nkperp to 2^14, the integration accuracy has worsened
 
 
