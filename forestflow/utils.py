88e72eea3748a626d71b2726e744a31a0848c23c
Andreu Font <font.andreu@gmail.com>
2026-04-26 07:59:55 -0700

    deleted old cosmo modules

--- forestflow/utils.py
+++ forestflow/utils.py
@@ -2,7 +2,6 @@ import numpy as np
 import torch
 import functools
 from pyDOE2 import lhs
-from lace.cosmo.camb_cosmo import dkms_dMpc
 
 
 def print_memory_usage(step_description):
@@ -115,34 +114,6 @@ def transform_arinyo_params(dict_arinyo_params, fcosmo):
         else:
             dict_arinyo_params_out[key] = dict_arinyo_params[key]
     return dict_arinyo_params_out
-
-
-def change_units(in_unit, out_unit, z, cosmo=None, camb_results=None):
-    lambda_alpha = 1215.67
-    clight = 299792.458
-    units = ["kms", "AA", "Mpc"]
-    # check in_unit and out_unit in units
-
-    if ((in_unit == "Mpc") | (out_unit == "Mpc")) & (cosmo is None):
-        raise ValueError("Please introduce cosmology")
-
-    if (in_unit == "AA") & (out_unit == "kms"):
-        k_convert = lambda_alpha * (1 + z) / clight
-    elif (in_unit == "kms") & (out_unit == "AA"):
-        k_convert = 1 / (lambda_alpha * (1 + z) / clight)
-    elif (in_unit == "kms") & (out_unit == "Mpc"):
-        k_convert = dkms_dMpc(cosmo, z, camb_results=camb_results)
-    elif (in_unit == "Mpc") & (out_unit == "kms"):
-        k_convert = 1 / dkms_dMpc(cosmo, z, camb_results=camb_results)
-    elif (in_unit == "AA") & (out_unit == "Mpc"):
-        AA2kms = lambda_alpha * (1 + z) / clight
-        kms2Mpc = dkms_dMpc(cosmo, z, camb_results=camb_results)
-        k_convert = AA2kms * kms2Mpc
-    elif (in_unit == "Mpc") & (out_unit == "AA"):
-        AA2kms = lambda_alpha * (1 + z) / clight
-        kms2Mpc = dkms_dMpc(cosmo, z, camb_results=camb_results)
-        k_convert = 1 / (AA2kms * kms2Mpc)
-    return k_convert
 
 
 def purge_chains(ln_prop_chains, nsplit=5, abs_diff=5, minval=-1000):
